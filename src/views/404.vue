795ae2e658392c8f4d4424935508f6ae193e87a9
Poison-Frog <46836317+Poison-Frog@users.noreply.github.com>
2020-10-16 02:09:09 +0800

    test:css

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,10 +1,10 @@
 <template>
-  <section class="b df" id="error-box">
-    <div class="mau">
-      <div class="fs24">404 Not Found</div>
+  <section class="error-page" id="error-page">
+    <div class="error-box">
+      <div class="error-text">404 Not Found</div>
       <div class="error-container">
         <div><img src="../assets/404.jpg" class="error-img"></div>
-        <div class="fs18">您输入的网址已被安澜吃掉，请更换正确的网址</div>
+        <div class="error-description">您输入的网址已被安澜吃掉，请更换正确的网址</div>
       </div>
     </div>
   </section>
@@ -15,10 +15,10 @@ import { defineComponent, onMounted, onUnmounted } from 'vue'
 export default defineComponent({
   setup () {
     const updateHeight = () => {
-      document.querySelector('#error-box').style.minHeight = window.innerHeight + 'px'
+      document.querySelector('#error-page').style.minHeight = window.innerHeight + 'px'
     }
     onMounted(() => {
-      document.querySelector('#error-box').style.minHeight = window.innerHeight + 'px'
+      document.querySelector('#error-page').style.minHeight = window.innerHeight + 'px'
       window.addEventListener('resize', updateHeight)
     })
     onUnmounted(() => {
@@ -29,11 +29,24 @@ export default defineComponent({
 </script>
 
 <style lang="less" scoped>
+.error-page {
+  .b;
+  .df
+}
+.error-box {
+  .mau
+}
+.error-text {
+  .fs24
+}
 .error-container {
   color: @red
 }
 .error-img {
   width: 373px;
   height: 320px
+}
+.error-description {
+  .fs18
 }
 </style>
